6f8bb3deeea81bb1e07e455a1df21a6eb47b2b94
Jakub Strakoš <StrakosJakub@sssvt.cz>
2025-12-30 16:02:15 +0100

    Added basic classses and a few changes

---
+++ Strakos_BackupClient/BackupMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Strakos_BackupClient
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum BackupMethod
+    {
+        Full,
+        Differential,
+        Incremental
+    }
+}
---
+++ Strakos_BackupClient/BackupRetention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Strakos_BackupClient
+{
+    public class BackupRetention
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+        [JsonPropertyName("size")]
+        public int Size { get; set; }
+
+    }
+}
