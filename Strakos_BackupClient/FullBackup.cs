3c96b7dce9b0497fc63c7c4c77bd192e48be2d4b
Jakub Strakoš <StrakosJakub@sssvt.cz>
2025-12-30 16:33:57 +0100

    Added FullBackup

---
+++ Strakos_BackupClient/FullBackup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strakos_BackupClient
+{
+    public class FullBackup : BackupAlgorithm
+    {
+        public FullBackup(BackupJob job) : base(job) { }
+        public override void Run()
+        {
+            foreach (var target in Job.Targets)
+            {
+                string backupFolder = CreateBackupFolder(target, "full");
+                foreach (var source in Job.Sources)
+                {
+                    Copy(source, backupFolder);
+                }
+                ApplyRetention(target);
+            }
+        }
+    }
+}
